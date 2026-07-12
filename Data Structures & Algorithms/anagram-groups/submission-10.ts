0973462aae2a3a7ec86f3886faa2d589cc588a64
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-12 12:24:33 +0530

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        if(strs.length === 0) return [];
+        const map = new Map<string, string[]>();
+        for(let str of strs){
+            const count = new Array(26).fill(0);
+            for(let i=0;i<str.length;i++){
+                count[str.charCodeAt(i) - 97]++;
+            }
+            const key = count.join("#");
+            if(!map.has(key)){
+                map.set(key, []);
+            }
+            const arr = map.get(key);
+            arr.push(str);
+            map.set(key, arr);
+        }
+        return [...map.values()];
+    }
+
+}
