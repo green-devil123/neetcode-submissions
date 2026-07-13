4cdcf5ab942dc89592e73cf363ba276c88a9abc7
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-13 14:51:18 +0530

    Add: longest-consecutive-sequence - submission-10

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-10.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const set =new Set<number>(nums);
+        let longest:number = 0;
+        let current:number = 0;
+        for(let num of nums){
+            current = num;
+            if(!set.has(current-1)){
+                let len = 1;
+                while(set.has(current+1)){
+                    current++;
+                    len++;
+                }
+                longest = Math.max(longest, len);
+            }
+        }
+        return longest;
+    }
+}
