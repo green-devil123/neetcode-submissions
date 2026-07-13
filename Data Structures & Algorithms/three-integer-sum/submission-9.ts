13993e64c8258327ec4495fd27a3598ca1c0e2d8
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-13 16:23:52 +0530

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        nums.sort((a,b)=>a-b)
+        let i:number =0, j:number = 0, k:number = 0;
+        let result: number[][] = [];
+        while(i<nums.length-2){
+            if(nums[i-1]!==undefined && nums[i] === nums[i-1]){
+                i++;
+                continue;
+            };
+            j=i+1;
+            k=nums.length-1;
+            while(j<k){
+                const sum = nums[i] + nums[j] + nums[k];
+                if(sum === 0){
+                    const arr:number[] = [nums[i], nums[j], nums[k]];
+                    result.push(arr);
+                    j++;
+                    k--;
+                    while(j<k && j-1 != i && nums[j] === nums[j-1]) j++;
+                    while(j<k &&  nums[k] === nums[k+1]) k--;
+                }else if(sum<0){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+            i++;
+        }
+        return result;
+    }
+}
