2bda4b6cb2c75da56d77ac4a93d26ac025e0c852
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-13 13:59:49 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = new Map<number,number>();
+        for(let i=0;i<nums.length;i++){
+            let diff = target-nums[i];
+            if(map.has(diff)){
+                const idx = map.get(diff);
+                return [idx, i];
+            }
+            map.set(nums[i], i);
+        }
+        return [-1,-1];
+    }
+}
