e095bcb9786ce93987997475adfb1713bd27ec60
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-15 14:22:01 +0530

    Add: sliding-window-maximum - submission-7

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-7.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums: number[], k: number): number[] {
+        const dq:number[] = [];
+        const result:number[] = [];
+        for(let i=0;i<nums.length;i++){
+            while(dq.length > 0 && dq[0] === i-k)dq.shift();
+
+            while(dq.length > 0 && nums[i] >= nums[dq[dq.length-1]]){
+                dq.pop();
+            }
+
+            dq.push(i);
+
+            if(i >= (k-1)) result.push(nums[dq[0]]);
+        }
+        return result;
+    }
+}
