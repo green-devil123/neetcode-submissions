733cf147e68936d9474b1168c10a00d9d00cb638
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-16 12:49:45 +0530

    Add: longest-substring-without-duplicates - submission-20

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-20.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let maxLen = 0;
+        const set = new Set<string>();
+        let left:number = 0;
+        for(let right=0;right<s.length;right++){
+            while(set.has(s[right])){
+                set.delete(s[left]);
+                left++;
+            }
+            set.add(s[right]);
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+        return maxLen;
+    }
+}
