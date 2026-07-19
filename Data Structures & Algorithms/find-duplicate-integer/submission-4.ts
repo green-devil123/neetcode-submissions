25131cd01c7c76d3fce48e3c01ee9c88bfb2a911
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-19 15:21:34 +0530

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums: number[]): number {
+        // const set = new Set();
+        // for(let num of nums){
+        //     if(set.has(num)) return num
+        //     set.add(num)
+        // }
+        // return -1
+        let slow = 0;
+        let fast = 0;
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow !== fast);
+
+        let ptr1 = 0;
+        let ptr2 = slow;
+
+        while (ptr1 !== ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+}
