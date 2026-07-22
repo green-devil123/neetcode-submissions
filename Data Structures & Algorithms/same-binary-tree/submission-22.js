e6f453321e2bd4a68ef1a9d352fdb341a861fa1a
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-22 17:07:24 +0530

    Add: same-binary-tree - submission-22

---
+++ Data Structures & Algorithms/same-binary-tree/submission-22.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p.val !== q.val) return false;
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right)
+        
+        // const queue = [];
+        // queue.push([p,q]);
+        // while(queue.length > 0){
+        //     const [n1, n2] = queue.shift();
+
+        //     if(!n1 && !n2) continue;
+
+        //     if(!n1 || !n2) return false;
+
+        //     if(n1.val !== n2.val) return false;
+
+        //     queue.push([n1.left, n2.left]);
+        //     queue.push([n1.right, n2.right])
+        // }
+        // return true;
+    }
+}
