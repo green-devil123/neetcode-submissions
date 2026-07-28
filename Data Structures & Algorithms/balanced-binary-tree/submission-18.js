24756705e05ba5f93c7d8468a5951e65cb088240
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-07-28 13:59:15 +0530

    Add: balanced-binary-tree - submission-18

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-18.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    findDepth(root){
+        if(!root) return 0;
+        const left = this.findDepth(root.left);
+        const right = this.findDepth(root.right);
+        if(left === -1  || right === -1) return -1;
+        if(Math.abs(left-right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        if(!root) return true;
+        // const ld = this.findDepth(root.left);
+        // const rd = this.findDepth(root.right);
+        // if(Math.abs(ld-rd) > 1) return false;
+        
+        // const left = this.isBalanced(root.left);
+        // const right = this.isBalanced(root.right);
+
+        // if(!left || !right) return false;
+        // return true;
+
+        return this.findDepth(root) !== -1;
+    }
+}
