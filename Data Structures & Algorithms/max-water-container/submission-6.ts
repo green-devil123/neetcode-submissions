1f1a7a7dff217af7b9ea4714088aa1cb600f2e83
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-08-02 14:29:12 +0530

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let left = 0, right = heights.length-1;
+        let maxArea = 0;
+        while(left < right){
+            let l = 0;
+            let b = right-left;
+            if(heights[left]<heights[right]){
+                l = heights[left];
+                left++;
+            }else if(heights[left] > heights[right]){
+                l = heights[right];
+                right--;
+            }else{
+                l = heights[right];
+                left++;
+                right--;
+            }
+            const area = b * l;
+            maxArea = Math.max(area, maxArea);
+        }
+        return maxArea;
+    }
+}
