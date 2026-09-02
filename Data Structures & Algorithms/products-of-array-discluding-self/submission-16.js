7d0eb6192f19a6b67b47cef902e2898191c9c162
Tarun Agarwal <40141179+green-devil123@users.noreply.github.com>
2026-09-02 11:09:44 +0530

    Add: products-of-array-discluding-self - submission-16

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-16.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const prefix = new Array(n).fill(1);
+        const suffix = new Array(n).fill(1);
+
+        for(let i=1;i<n;i++){
+            prefix[i] = nums[i-1]*prefix[i-1];
+        }
+        for(let i=n-2;i>=0;i--){
+            suffix[i] = nums[i+1] * suffix[i+1]
+        }
+
+        for(let i=0;i<n;i++){
+            prefix[i] = prefix[i]*suffix[i];
+        }
+
+        return prefix;
+    }
+}
